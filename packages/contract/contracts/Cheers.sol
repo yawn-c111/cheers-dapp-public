230ce65eecae2f95412d285741a4bead6a5f143b
junya-thinkactive-d <junya.kodate@gmail.com>
2022-09-09 17:07:15 +0900

    feat:first frame

---
+++ packages/contract/contracts/Cheers.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./interfaces/ICheers.sol";
+
+contract Cheers is ICheers {
+  
+  constructor(){
+
+  }
+
+  // DAOプール作成
+  function newDaoPoolFactory () {
+
+  }
+  // DAOリスト追加
+  function addDaoList(){
+
+  }
+  // DAOリスト取得
+  function getAllDaoList(){
+
+  }
+
+  // Userプール作成
+  function newUserPoolFactory () {
+
+  }
+  // Userリスト追加
+  function addUserList(){
+
+  }
+  // Userリスト取得
+  function getAllUserList(){
+
+  }
+}
---
+++ packages/contract/contracts/DaoPool.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./interfaces/IDaoPool.sol";
+
+contract DaoPool is DaoPool {
+
+}
