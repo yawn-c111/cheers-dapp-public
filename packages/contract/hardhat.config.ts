1036c486e639681af062ce29e9b5c51865ec6dd7
junya-thinkactive-d <junya.kodate@gmail.com>
2022-10-11 15:20:19 +0900

    feat:add size-contracts

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@typechain/hardhat'
+import 'hardhat-contract-sizer'
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 
