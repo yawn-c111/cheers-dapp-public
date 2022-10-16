d57710976c2871ec93b828bfe83d3d267550d3fe
junya-thinkactive-d <develop.junya@gmail.com>
2022-10-16 17:56:41 +0900

    feat: level guage

--- packages/client/src/components/pages/UsersInfo.tsx
+++ packages/client/src/components/pages/UsersInfo.tsx
@@ -6,7 +6,7 @@ const UsersInfo = () => {
   return (
     <div className="flex justify-start w-full pl-12">
       {usersData.map((userData, i) => (
-          <UserCard key={i} userData={userData} />
+        <UserCard key={i} userData={userData} />
       ))}
     </div>
   );
