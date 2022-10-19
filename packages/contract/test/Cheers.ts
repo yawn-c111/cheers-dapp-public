117a80b4149e0c2d7864af61e72772ea9c046398
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-10-19 19:22:22 +0900

    feat: update Cheers.ts

--- packages/contract/test/Cheers.ts
+++ packages/contract/test/Cheers.ts
@@ -1,4 +1,4 @@
-import { time, loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
@@ -13,8 +13,28 @@ describe('Cheers', function () {
   }
 
   describe('Run test', function () {
-    it('Should run test', async () => {
-      console.log('run test');
+    it('Should run', async () => {
     });
   });
+
+  describe('Deploy test', function () {
+    it('Should deploy', async () => {
+      const [deployer, user1] = await ethers.getSigners();
+  
+      const cheersFactory = await ethers.getContractFactory('Cheers');
+      const cheers = await cheersFactory.deploy();
+    });
+
+    it('Should load fixture', async () => {
+      const { cheers, deployer, user1 } = await loadFixture(fixture);
+    });
+  });
+
+  // describe('newDaoPoolFactory test', function () {
+  //   it('Should newDaoPoolFactory', async () => {
+  //     const { cheers, deployer, user1 } = await loadFixture(fixture);
+  //     let newDaoPoolFactory = await cheers.newDaoPoolFactory("DAO1_Name", "DAO1_Profile", "DAO1_Icon");
+  //     await newDaoPoolFactory.wait();
+  //   });
+  // });
 });
