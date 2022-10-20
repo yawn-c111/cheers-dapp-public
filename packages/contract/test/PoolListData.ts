6c8f4e65bedf92ec7071c2549fa5521a61af4bf4
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-10-20 21:05:13 +0900

    feat: add test PoolListData.ts

---
+++ packages/contract/test/PoolListData.ts
@@ -0,0 +1,83 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { PoolListData } from '../types'
+
+describe('PoolListData', function () {
+
+  let poolListData: PoolListData;
+
+  async function fixture() {
+    const [deployer, user1, pool1, pool2] = await ethers.getSigners();
+
+    const poolListDataFactory = await ethers.getContractFactory('PoolListData');
+    poolListData = await poolListDataFactory.deploy();
+    await poolListData.deployed();
+
+    return { poolListData, deployer, user1, pool1, pool2 };
+  }
+
+  describe('Run test', function () {
+    it('Should run', async () => {
+    });
+  }); 
+
+  describe('Deploy test', function () {
+    it('Should deploy', async () => {
+      const [deployer, user1, pool1, pool2] = await ethers.getSigners();
+  
+      const poolListDataFactory = await ethers.getContractFactory('PoolListData');
+      const poolListData = await poolListDataFactory.deploy();
+    });
+
+    it('Should load fixture', async () => {
+      const { poolListData, deployer, user1, pool1, pool2 } = await loadFixture(fixture);
+    });
+  });
+
+  describe('getMyPoolAddress test', function () {
+    it("Should initial poolAddress be zero address", async () => {
+      const { poolListData, user1 } = await loadFixture(fixture);
+
+      let getMyPoolAddress = await poolListData.getMyPoolAddress(user1.address);
+      expect(getMyPoolAddress).to.equal(ethers.constants.AddressZero);
+    });
+  });
+
+  describe('addMyPoolAddress test', function () {
+    it("Should add poolAddress", async () => {
+      const { poolListData, user1, pool1 } = await loadFixture(fixture);
+
+      let getMyPoolAddress;
+      let addMyPoolAddress;
+
+      getMyPoolAddress = await poolListData.getMyPoolAddress(user1.address);
+      expect(getMyPoolAddress).to.equal(ethers.constants.AddressZero);
+
+      addMyPoolAddress = await poolListData.addMyPoolAddress(user1.address, pool1.address);
+      await addMyPoolAddress.wait();
+
+      getMyPoolAddress = await poolListData.getMyPoolAddress(user1.address);
+      expect(getMyPoolAddress).to.equal(pool1.address);
+    });
+
+    it("Should change poolAddress", async () => {
+      const { poolListData, user1, pool1, pool2 } = await loadFixture(fixture);
+
+      let getMyPoolAddress;
+      let addMyPoolAddress;
+
+      addMyPoolAddress = await poolListData.addMyPoolAddress(user1.address, pool1.address);
+      await addMyPoolAddress.wait();
+
+      getMyPoolAddress = await poolListData.getMyPoolAddress(user1.address);
+      expect(getMyPoolAddress).to.equal(pool1.address);
+
+      addMyPoolAddress = await poolListData.addMyPoolAddress(user1.address, pool2.address);
+      await addMyPoolAddress.wait();
+
+      getMyPoolAddress = await poolListData.getMyPoolAddress(user1.address);
+      expect(getMyPoolAddress).to.equal(pool2.address);
+    });
+  });
+});
