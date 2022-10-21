e4d8474e7b9f4575ffdb81a7131461bdf3ac91ad
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-10-21 15:23:30 +0900

    feat: update Cheers.ts

--- packages/contract/test/Cheers.ts
+++ packages/contract/test/Cheers.ts
@@ -1,15 +1,18 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { Cheers, DaoPoolFactory, UserPoolFactory, PoolListData, DaosData } from '../types'
+import { Cheers, DaoPoolFactory, UserPoolFactory, PoolListData, DaosData, UsersData } from '../types'
 
 describe('Cheers', function () {
 
   let cheers: Cheers;
+
   let daoPoolFactory: DaoPoolFactory;
   let userPoolFactory: UserPoolFactory;
+
   let poolListData: PoolListData;
   let daosData: DaosData;
+  let usersData: UsersData;
 
   async function fixture() {
     const [deployer, user1] = await ethers.getSigners();
@@ -23,6 +26,10 @@ describe('Cheers', function () {
     daoPoolFactory = await daoPoolFactoryFactory.deploy();
     await daoPoolFactory.deployed();
 
+    const UserPoolFactoryFactory = await ethers.getContractFactory('UserPoolFactory');
+    userPoolFactory = await UserPoolFactoryFactory.deploy();
+    await userPoolFactory.deployed();
+
     const poolListDataFactory = await ethers.getContractFactory('PoolListData');
     poolListData = await poolListDataFactory.deploy();
     await poolListData.deployed();
@@ -31,17 +38,16 @@ describe('Cheers', function () {
     daosData = await daosDataFactory.deploy();
     await daosData.deployed();
 
-
-    const UserPoolFactoryFactory = await ethers.getContractFactory('UserPoolFactory');
-    userPoolFactory = await UserPoolFactoryFactory.deploy();
-    await userPoolFactory.deployed();
+    const usersDataFactory = await ethers.getContractFactory('UsersData');
+    usersData = await usersDataFactory.deploy();
+    await usersData.deployed();
 
 
     await (await cheers.setDaoPoolFactory(daoPoolFactory.address)).wait();
+    await (await cheers.setUserPoolFactory(userPoolFactory.address)).wait();
     await(await daoPoolFactory.setPoolListData(poolListData.address)).wait();
     await(await daoPoolFactory.setDaosData(daosData.address)).wait();
-
-    await (await cheers.setUserPoolFactory(userPoolFactory.address)).wait();
+    await(await userPoolFactory.setUsersData(usersData.address)).wait();
 
     return { cheers, deployer, user1 };
   }
@@ -57,6 +63,14 @@ describe('Cheers', function () {
       const { cheers, user1 } = await loadFixture(fixture);
       let newDaoPoolFactory = await cheers.connect(user1).newDaoPoolFactory("DAO1_Name", "DAO1_Profile", "DAO1_Icon");
       await newDaoPoolFactory.wait();
+    });
+  });
+
+  describe('newUserPoolFactory test', function () {
+    it("Should create a new user's pool", async () => {
+      const { cheers, user1 } = await loadFixture(fixture);
+      let newUserPoolFactory = await cheers.connect(user1).newUserPoolFactory("USER1_Name", "USER1_Profile", "USER1_Icon");
+      await newUserPoolFactory.wait();
     });
   });
 });
