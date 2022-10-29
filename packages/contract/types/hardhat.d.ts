e4c84e6ec702c521b6bd81e75f11a7f2eac61ece
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-10-30 01:17:09 +0900

    feat: add allProjectsList and functions in ProjectsData.sol

--- packages/contract/types/hardhat.d.ts
+++ packages/contract/types/hardhat.d.ts
@@ -13,6 +13,22 @@ import * as Contracts from ".";
 declare module "hardhat/types/runtime" {
   interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
     getContractFactory(
+      name: "Ownable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.Ownable__factory>;
+    getContractFactory(
+      name: "ERC20",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC20__factory>;
+    getContractFactory(
+      name: "IERC20Metadata",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC20Metadata__factory>;
+    getContractFactory(
+      name: "IERC20",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC20__factory>;
+    getContractFactory(
       name: "Cheers",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Cheers__factory>;
@@ -77,6 +93,10 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IUsersData__factory>;
     getContractFactory(
+      name: "CHERToken",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.CHERToken__factory>;
+    getContractFactory(
       name: "ProjectPool",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ProjectPool__factory>;
@@ -85,6 +105,26 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.UserPool__factory>;
 
+    getContractAt(
+      name: "Ownable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Ownable>;
+    getContractAt(
+      name: "ERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC20>;
+    getContractAt(
+      name: "IERC20Metadata",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC20Metadata>;
+    getContractAt(
+      name: "IERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC20>;
     getContractAt(
       name: "Cheers",
       address: string,
@@ -165,6 +205,11 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.IUsersData>;
+    getContractAt(
+      name: "CHERToken",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CHERToken>;
     getContractAt(
       name: "ProjectPool",
       address: string,
