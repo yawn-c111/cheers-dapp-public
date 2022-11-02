15c387f01437e8c2b1854dec1b388b611546caf3
junya-thinkactive-d <develop.junya@gmail.com>
2022-11-02 17:22:01 +0900

    fix: some fix for vercel 3

--- packages/client/src/components/shared/parts/CheerProjects.tsx
+++ packages/client/src/components/shared/parts/CheerProjects.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { CheerCard } from '@/components/shared/card';
-import { cheersData } from '@/data';
+// import { CheerCard } from '@/components/shared/card';
+// import { cheersData } from '@/data';
 
 const CheerProjects = () => {
   return (
     <div className="p-12">
       <div className="text-4xl mb-12">Cheer Projects</div>
       <div className="flex flex-wrap">
-        {cheersData.map((cheer, i) => (
+        {/* {cheersData.map((cheer, i) => (
           <CheerCard
             key={i}
             challengerName={cheer.challengerName}
@@ -18,7 +18,7 @@ const CheerProjects = () => {
             comment={cheer.comment}
             throwCher={cheer.throwCher}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   );
